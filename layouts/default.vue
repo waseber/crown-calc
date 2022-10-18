820cd66694189b62762263f52c876eadec433855
waseber <yvesgebrer@gmail.com>
2022-10-18 11:11:45 -0400

    added tailwind

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container mx-auto">
     <header>
       <slot name="header" />
     </header>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,18 @@
-import { defineNuxtConfig } from 'nuxt3'
+// import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
-export default defineNuxtConfig({
-
-})
+export default {
+    build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  },
+  css: [
+    "~/assets/css/tailwind.css"
+  ],
+}
